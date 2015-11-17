09217054707b01e656e6acfc7de41eaa95df8b6a
Marco Semiao <marco.semiao@gmail.com>
2015-11-17 17:16:45 +0100

    fake implementation comment

--- com.ibm.ws.jpa/src/main/java/com/ibm/ws/persistence/jdbc/sql/DB2Dictionary.java
+++ com.ibm.ws.jpa/src/main/java/com/ibm/ws/persistence/jdbc/sql/DB2Dictionary.java
@@ -1,5 +1,8 @@
 package com.ibm.ws.persistence.jdbc.sql;
 
+/**
+ * Fake IBM Implementation - Only use for compilation phase
+ */
 public class DB2Dictionary extends org.apache.openjpa.jdbc.sql.DB2Dictionary {
 
 }
