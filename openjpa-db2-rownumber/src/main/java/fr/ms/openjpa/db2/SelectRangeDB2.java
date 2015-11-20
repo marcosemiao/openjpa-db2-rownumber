f90125ea42c43016dd37cc817f7208be77341ac9
Marco Semiao <marco.semiao@gmail.com>
2015-11-20 11:51:55 +0100

    condition uniquement quand cela est utile si start ==0 et end= max

--- openjpa-db2-rownumber/src/main/java/fr/ms/openjpa/db2/SelectRangeDB2.java
+++ openjpa-db2-rownumber/src/main/java/fr/ms/openjpa/db2/SelectRangeDB2.java
@@ -31,13 +31,26 @@ final class SelectRangeDB2 {
     static void appendSelectRange(final SQLBuffer buf, final long start, final long end, final boolean subselect) {
 
 	if (!subselect) {
-	    final StringBuilder builder = new StringBuilder(
-		    "SELECT * FROM (SELECT rr.*, ROW_NUMBER() OVER(ORDER BY ORDER OF rr) AS rn FROM (");
+	    final StringBuilder builder = new StringBuilder("SELECT * FROM (SELECT rr.*, ROW_NUMBER() OVER(ORDER BY ORDER OF rr) AS rn FROM (");
 	    builder.append(buf.getSQL());
-	    builder.append(") AS rr) AS r WHERE rn > ");
-	    builder.append(start);
-	    builder.append(" AND rn <= ");
-	    builder.append(end);
+	    builder.append(") AS rr) AS r WHERE ");
+
+	    final boolean isStart = (start != 0);
+	    final boolean isEnd = (end != Long.MAX_VALUE);
+
+	    if (isStart) {
+		builder.append("rn > ");
+		builder.append(start);
+	    }
+
+	    if (isStart && isEnd) {
+		builder.append(" AND ");
+	    }
+
+	    if (isEnd) {
+		builder.append("rn <= ");
+		builder.append(end);
+	    }
 
 	    buf.replaceSqlString(0, buf.getSQL().length(), builder.toString());
 	}
